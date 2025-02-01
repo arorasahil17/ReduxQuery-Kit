9fd7816ccbafa860490f3dbacca84ad4319ab8e2
sahil <arorasahil074@gmail.com>
2025-02-01 15:00:44 +0530

    updated exec script

--- index.cjs
+++ index.cjs
@@ -4,7 +4,11 @@ const { execSync } = require("child_process");
 const projectName = process.argv[2] || "my-app";
 
 console.log(`Creating project: ${projectName}`);
-execSync(`git clone <github-repo-url> ${projectName}`);
+console.log(`echo Creating project: ${projectName}`);
+execSync(
+  `git clone https://github.com/arorasahil17/ReduxQuery-Kit.git ${projectName}`
+);
 execSync(`cd ${projectName} && rm -rf .git && npm install`, {
   stdio: "inherit",
 });
+execSync(`echo Installing dependencies...`, { stdio: "inherit" });
